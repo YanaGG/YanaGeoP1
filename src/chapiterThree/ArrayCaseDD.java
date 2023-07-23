8ef26f9d8d91cb7f7410e1f4b7294329a691fe92
Yana Geo <94054850+YanaGG@users.noreply.github.com>
2023-07-23 18:59:00 +0300

    Edited

--- src/chapiterThree/ArrayCaseDD.java
+++ src/chapiterThree/ArrayCaseDD.java
@@ -1,2 +1,22 @@
-package chapiterThree;public class ArrayCaseDD {
+package chapiterThree;
+
+public class ArrayCaseDD {
+    public static void main(String[] args) {
+
+        char symbol = 'a';
+        String[][] myArray = new String[5][7];
+
+        for (int i = 0; i < myArray.length; i++) {
+            for (int j = 0; j < myArray[4].length; j++) {
+                myArray[i][j] = symbol + "" + (j + 1);
+                System.out.print(myArray[i][j] + " ");
+            }
+            symbol++;
+            System.out.println();
+
+
+
+        }
+
+    }
 }
